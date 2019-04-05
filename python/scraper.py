2fdf18ff5a5f0f8d3dd589375006178cb0a165a7
vishal300 <2492vishalsingh@gmail.com>
2019-04-05 12:40:19 +0530

    First version of scraping 1.0

--- python/scraper.py
+++ python/scraper.py
@@ -1,12 +1,16 @@
 from urllib.request import Request, urlopen
+import re
 req = Request('https://medium.com/', headers={'User-Agent': 'Mozilla/5.0'})
 the_page =  urlopen(req).read()
 print(the_page)
-
-Sring = the_page.decode()
+the_page=str(the_page)
+links = re.findall('"((http)s?://.*?)"', the_page)
 #Some how extract that wanted data
 
+print(links)
+links=str(links)
 f = open('output.txt', 'w')
-f.write(Sring)
+f.write(links)
 f.close()
+
           
